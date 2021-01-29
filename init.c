ebc7c428399cf6f130ff63e5ec87b237b45650c5
akochenkov <alexandr.kochenkov@gmail.com>
2021-01-28 17:59:00 -0800

    Put in the comments.

--- init.c
+++ init.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 struct node *init() {
-  struct node *s=malloc(sizeof(struct node));
-//  s->next=NULL;
+  struct node *s=malloc(sizeof(struct node));  //this allocates a block og memory for s
+  s->next=NULL;  //makes the sentinel node s next be NULL
   return(s);
 
 }
